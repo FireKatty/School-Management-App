bdfa143740ed8228834249d5d2fe1e90e3515351
firekatty <abhishekkatiyar1947@gmail.com>
2024-12-29 19:22:46 +0530

    Update connectDatabase.js

--- backend/db/connectDatabase.js
+++ backend/db/connectDatabase.js
@@ -3,7 +3,7 @@ const mongoUri = process.env.MONGO_URI;
 
 const connectToDatabase = async()=>{
     try {
-        await mongoose.connect('mongodb://localhost:27017/School-Management-App', { useNewUrlParser: true, useUnifiedTopology: true });
+        await mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
         console.log("Database Connected")
     } catch (error) {
         console.log("Error")
